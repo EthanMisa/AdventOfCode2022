9467ecd96e22df51422c7c3179ab4f89d6df906a
Ethan Misa <eamisa@uwaterloo.ca>
2022-12-02 02:39:47 -0500

    created template

---
+++ day3/day3a.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <map>
+#include <string>
+#include <sstream>
+using namespace std;
+
+int main() {
+    string s;
+    int total = 0;
+    while (!cin.eof()){
+
+    }
+    cout << "The total is: " << total << endl;
+}
